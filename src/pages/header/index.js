3005ef7b70544e35953a12fd8c55d45618aa3df6
JohnnySantosDev <62815361+JohnnySantosDev@users.noreply.github.com>
2023-03-24 13:14:01 -0400

    Update index.js

--- src/pages/header/index.js
+++ src/pages/header/index.js
@@ -6,7 +6,10 @@ export default function Navbar() {
   const [isNavExpanded, setIsNavExpanded] = useState(false)
 
   return (
+    
     <div className="HeaderPai">
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2811782341555306"
+     crossorigin="anonymous"></script>
         <nav className="navigation">
         
           <button
